7d1e4801ffbcbcf984e769e6c57ab5b7c333eb45
Colin Waddell <mrcolin@gmail.com>
2013-08-22 22:00:07 +0100

    fixed FF bug for slider

--- templates/content-home.php
+++ templates/content-home.php
@@ -11,11 +11,7 @@
 
       </div>  
       <div class="col-sm-6">
-        <div class="v-center-outer">
-           <div class="v-center-inner">
-              <?php echo do_shortcode('[image-carousel showcaption="false" twbs="3" interval="1200" category="homepage"]'); ?>
-           </div>
-        </div>
+        <?php echo do_shortcode('[image-carousel showcaption="false" twbs="3" interval="1200" category="homepage"]'); ?>
       </div>  
     </div>
   </figure>
